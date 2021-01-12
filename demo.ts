e8da740d0d727f6274a4f778d0a2d514e5702c7e
swfz <sawafuji.09@gmail.com>
2021-01-13 04:08:23 +0900

    feat: format

--- demo.ts
+++ demo.ts
@@ -3,14 +3,17 @@ type Row = {
   value: number;
 };
 const rows: Row[] = [
-  { name: 'foo', value: 1 },
-  { name: 'bar', value: 2 },
-  { name: 'baz', value: 3 },
-  { name: 'bar', value: 2 },
+  { name: "foo", value: 1 },
+  { name: "bar", value: 2 },
+  { name: "baz", value: 3 },
+  { name: "bar", value: 2 },
 ];
 const summary = rows.reduce<Record<string, Row>>((acc, current) => {
   const key = current.name;
-  return { ...acc, [key]: { ...acc[key], value: (acc[key]?.value ?? 0) + current.value } };
+  return {
+    ...acc,
+    [key]: { ...acc[key], value: (acc[key]?.value ?? 0) + current.value },
+  };
 }, {});
 
 console.log(summary); // { foo: { value: 1 }, bar: { value: 4 }, baz: { value: 3 } }
--- demo2.ts
+++ demo2.ts
@@ -1,45 +1,50 @@
 type Row = {
-  name: 'foo'|'bar'|'baz';
+  name: "foo" | "bar" | "baz";
   date: string;
   column1: string;
   value: number;
 };
 
 type AggregatedRow = {
-  name: 'foo'|'bar'|'baz';
+  name: "foo" | "bar" | "baz";
   // name: string;
   date: string;
   sum: number;
-}
+};
 
 type Summary = {
   foo: Record<string, AggregatedRow>;
   bar: Record<string, AggregatedRow>;
   baz: Record<string, AggregatedRow>;
-}
+};
 
 const rows: Row[] = [
-  {name: 'foo', date: '2021-01-02', column1: 'hoge', value: 2},
-  {name: 'bar', date: '2021-01-02', column1: 'hoge', value: 2},
-  {name: 'baz', date: '2021-01-02', column1: 'hoge', value: 3},
-  {name: 'foo', date: '2021-01-03', column1: 'hoge', value: 1},
-  {name: 'bar', date: '2021-01-03', column1: 'hoge', value: 2},
-  {name: 'baz', date: '2021-01-03', column1: 'hoge', value: 1},
-  {name: 'foo', date: '2021-01-02', column1: 'fuga', value: 2},
-  {name: 'bar', date: '2021-01-02', column1: 'fuga', value: 1},
-  {name: 'baz', date: '2021-01-02', column1: 'fuga', value: 3},
-  {name: 'foo', date: '2021-01-03', column1: 'fuga', value: 6},
-  {name: 'bar', date: '2021-01-03', column1: 'fuga', value: 2},
-  {name: 'baz', date: '2021-01-03', column1: 'fuga', value: 3},
+  { name: "foo", date: "2021-01-02", column1: "hoge", value: 2 },
+  { name: "bar", date: "2021-01-02", column1: "hoge", value: 2 },
+  { name: "baz", date: "2021-01-02", column1: "hoge", value: 3 },
+  { name: "foo", date: "2021-01-03", column1: "hoge", value: 1 },
+  { name: "bar", date: "2021-01-03", column1: "hoge", value: 2 },
+  { name: "baz", date: "2021-01-03", column1: "hoge", value: 1 },
+  { name: "foo", date: "2021-01-02", column1: "fuga", value: 2 },
+  { name: "bar", date: "2021-01-02", column1: "fuga", value: 1 },
+  { name: "baz", date: "2021-01-02", column1: "fuga", value: 3 },
+  { name: "foo", date: "2021-01-03", column1: "fuga", value: 6 },
+  { name: "bar", date: "2021-01-03", column1: "fuga", value: 2 },
+  { name: "baz", date: "2021-01-03", column1: "fuga", value: 3 },
 ];
 
 const aggregateFn = (acc: Summary, current: Row): Summary => {
   const name = current.name;
   const date = current.date;
-  const aggregated = acc?.[name]?.[date] ? {...acc[name][date], sum: acc[name][date].sum + current.value} : {...{name,date}, sum: current.value}
-  const updateValue: Record<string, AggregatedRow> = {...acc[name], [date]: aggregated };
+  const aggregated = acc?.[name]?.[date]
+    ? { ...acc[name][date], sum: acc[name][date].sum + current.value }
+    : { ...{ name, date }, sum: current.value };
+  const updateValue: Record<string, AggregatedRow> = {
+    ...acc[name],
+    [date]: aggregated,
+  };
 
-  return {...acc, [name]: updateValue};
+  return { ...acc, [name]: updateValue };
 };
 
 const summary = rows.reduce(aggregateFn, {} as Summary);
