6e52e7443ac7cf65dbe1a94a3af5e35358fc1f44
swfz <sawafuji.09@gmail.com>
2021-01-13 04:08:15 +0900

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ]
+}
